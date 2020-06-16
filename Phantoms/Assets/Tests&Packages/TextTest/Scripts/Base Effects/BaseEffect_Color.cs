de5d57fe1c5a814b6e22d03371b2c7b41433faf8
CJ Desktop <cjlegato@gmail.com>
2020-06-16 16:25:19 -0400

    Updated base color effect to add more methods of blending colors.

--- Phantoms/Assets/Tests&Packages/TextTest/Scripts/Base Effects/BaseEffect_Color.cs
+++ Phantoms/Assets/Tests&Packages/TextTest/Scripts/Base Effects/BaseEffect_Color.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Color", menuName = "FancyText/BaseEffect/Color", order = 2)]
 public class BaseEffect_Color : BaseEffect
 {
+    private enum ColorBlendMode
+    {
+        SET,        // Simply sets the color to the value
+        MULTIPLY,   // Multiplies the two colors together.
+        ADD         // Adds the two colors together.
+    }
+
+    [SerializeField]
+    [Tooltip("Chooses the method of blending colors.")]
+    private ColorBlendMode m_colorBlendMode = ColorBlendMode.SET;
+
     [SerializeField]
     [Tooltip("Chooses how much to blend the colors - with 0 just being the previous color, and 1 being the new color.")]
     [Range(0f, 1f)]
-    private float m_colorBlend = 1f;
+    private float m_colorBlendStrength = 1f;
 
     [SerializeField]
     [Tooltip("The color gradient to animate the text over")]
@@ -17,13 +28,30 @@ public class BaseEffect_Color : BaseEffect
     protected override void ApplyEffect(float progress, int vertexIndex, Vector3[] sourceVertices, ref Vector3[] destinationVertices, ref Color32[] newVertexColors)
     {
         Color color = m_colorValue.Evaluate(progress);
-        Color32 displayColor = color;
 
         int[] vertices = m_vertices.Vertices();
         for (int i = 0; i < vertices.Length; i++)
         {
             int vert = vertices[i];
-            newVertexColors[vertexIndex + vert] = Color32.Lerp(newVertexColors[vertexIndex + vert], displayColor, m_colorBlend);
+
+            Color defaultColor = newVertexColors[vertexIndex + vert];
+
+            Color targetColor = color;
+
+            switch (m_colorBlendMode)
+            {
+                case ColorBlendMode.MULTIPLY:
+                    targetColor = defaultColor * color;
+                    break;
+                case ColorBlendMode.ADD:
+                    targetColor = defaultColor + color;
+                    break;
+                case ColorBlendMode.SET:
+                    // Do nothing, leave target color as default;
+                    break;
+            }
+
+            newVertexColors[vertexIndex + vert] = Color32.Lerp(defaultColor, targetColor, m_colorBlendStrength);
         }
     }
 }
