9b4b21df36a75dcfa69399284f2be1fab6d73907
CJ Desktop <cjlegato@gmail.com>
2020-06-24 17:51:33 -0400

    Updating the player camera to have a consitent angle while still following the player.

--- Phantoms/Assets/Scripts/Player/CameraController.cs
+++ Phantoms/Assets/Scripts/Player/CameraController.cs
@@ -4,173 +4,56 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [Header("Framing")]
-    public Camera Camera;
-    public Vector2 FollowPointFraming = new Vector2(0f, 0f);
-    public float FollowingSharpness = 10000f;
+    [Header("Player Camera Variables")]
+    [SerializeField]
+    private float m_distanceFromFocus = 10f;
 
-    [Header("Distance")]
-    public float DefaultDistance = 6f;
-    public float MinDistance = 0f;
-    public float MaxDistance = 10f;
-    public float DistanceMovementSpeed = 5f;
-    public float DistanceMovementSharpness = 10f;
+    [SerializeField]
+    private Vector3 m_cameraForward = Vector3.forward;
 
-    [Header("Rotation")]
-    public bool InvertX = false;
-    public bool InvertY = false;
-    [Range(-90f, 90f)]
-    public float DefaultVerticalAngle = 20f;
-    [Range(-90f, 90f)]
-    public float MinVerticalAngle = -90f;
-    [Range(-90f, 90f)]
-    public float MaxVerticalAngle = 90f;
-    public float RotationSpeed = 1f;
-    public float RotationSharpness = 10000f;
 
-    [Header("Obstruction")]
-    public float ObstructionCheckRadius = 0.2f;
-    public LayerMask ObstructionLayers = -1;
-    public float ObstructionSharpness = 10000f;
-    public List<Collider> IgnoredColliders = new List<Collider>();
 
-    public Transform Transform { get; private set; }
-    public Transform FollowTransform { get; private set; }
+    [SerializeField]
+    private float m_angleUp = 30f;
 
-    public Vector3 PlanarDirection { get; set; }
-    public float TargetDistance { get; set; }
+    [SerializeField]
+    private float m_leadDistance = 2f;
 
-    private bool _distanceIsObstructed;
-    private float _currentDistance;
-    private float _targetVerticalAngle;
-    private RaycastHit _obstructionHit;
-    private int _obstructionCount;
-    private RaycastHit[] _obstructions = new RaycastHit[MaxObstructions];
-    private float _obstructionTime;
-    private Vector3 _currentFollowPosition;
 
-    private const int MaxObstructions = 32;
+    [HideInInspector]
+    public Transform Player = null;
 
-    void OnValidate()
+
+    private float m_currentLead = 0f;
+    private Vector3 m_focusPosition = Vector3.zero;
+
+
+
+    private void LateUpdate()
     {
-        DefaultDistance = Mathf.Clamp(DefaultDistance, MinDistance, MaxDistance);
-        DefaultVerticalAngle = Mathf.Clamp(DefaultVerticalAngle, MinVerticalAngle, MaxVerticalAngle);
+        // First get camera focus position
+        UpdateFocus();
+        // Then set the camera position based on the focus.
+        UpdateCamera();
     }
 
-    void Awake()
+
+    private void UpdateFocus()
     {
-        Transform = this.transform;
-
-        _currentDistance = DefaultDistance;
-        TargetDistance = _currentDistance;
-
-        _targetVerticalAngle = 0f;
-
-        PlanarDirection = Vector3.forward;
+        m_focusPosition = Player.position;
     }
 
-    // Set the transform that the camera will orbit around
-    public void SetFollowTransform(Transform t)
+    private void UpdateCamera()
     {
-        FollowTransform = t;
-        PlanarDirection = FollowTransform.forward;
-        _currentFollowPosition = FollowTransform.position;
-    }
 
-    public void UpdateWithInput(float deltaTime, float zoomInput, Vector3 rotationInput)
-    {
-        if (FollowTransform)
-        {
-            if (InvertX)
-            {
-                rotationInput.x *= -1f;
-            }
-            if (InvertY)
-            {
-                rotationInput.y *= -1f;
-            }
+        // Note: Assuming that the camera up will always just be Vector3.up
 
-            // Process rotation input
-            Quaternion rotationFromInput = Quaternion.Euler(FollowTransform.up * (rotationInput.x * RotationSpeed));
-            PlanarDirection = rotationFromInput * PlanarDirection;
-            PlanarDirection = Vector3.Cross(FollowTransform.up, Vector3.Cross(PlanarDirection, FollowTransform.up));
-            _targetVerticalAngle -= (rotationInput.y * RotationSpeed);
-            _targetVerticalAngle = Mathf.Clamp(_targetVerticalAngle, MinVerticalAngle, MaxVerticalAngle);
+        Vector3 cameraOffset = Vector3.RotateTowards(m_cameraForward * -1f, Vector3.up, Mathf.Deg2Rad * m_angleUp, 0f) * m_distanceFromFocus;
+
+        transform.position = m_focusPosition + cameraOffset;
 
 
-            // Process distance input
-            if (_distanceIsObstructed && Mathf.Abs(zoomInput) > 0f)
-            {
-                TargetDistance = _currentDistance;
-            }
-            TargetDistance += zoomInput * DistanceMovementSpeed;
-            TargetDistance = Mathf.Clamp(TargetDistance, MinDistance, MaxDistance);
-
-            // Find the smoothed follow position
-            _currentFollowPosition = Vector3.Lerp(_currentFollowPosition, FollowTransform.position, 1f - Mathf.Exp(-FollowingSharpness * deltaTime));
-
-            // Calculate smoothed rotation
-            Quaternion planarRot = Quaternion.LookRotation(PlanarDirection, FollowTransform.up);
-            Quaternion verticalRot = Quaternion.Euler(_targetVerticalAngle, 0, 0);
-            Quaternion targetRotation = Quaternion.Slerp(Transform.rotation, planarRot * verticalRot, 1f - Mathf.Exp(-RotationSharpness * deltaTime));
-
-            // Apply rotation
-            Transform.rotation = targetRotation;
-
-            // Handle obstructions
-            {
-                RaycastHit closestHit = new RaycastHit();
-                closestHit.distance = Mathf.Infinity;
-                _obstructionCount = Physics.SphereCastNonAlloc(_currentFollowPosition, ObstructionCheckRadius, -Transform.forward, _obstructions, TargetDistance, ObstructionLayers, QueryTriggerInteraction.Ignore);
-                for (int i = 0; i < _obstructionCount; i++)
-                {
-                    bool isIgnored = false;
-                    for (int j = 0; j < IgnoredColliders.Count; j++)
-                    {
-                        if (IgnoredColliders[j] == _obstructions[i].collider)
-                        {
-                            isIgnored = true;
-                            break;
-                        }
-                    }
-                    for (int j = 0; j < IgnoredColliders.Count; j++)
-                    {
-                        if (IgnoredColliders[j] == _obstructions[i].collider)
-                        {
-                            isIgnored = true;
-                            break;
-                        }
-                    }
-
-                    if (!isIgnored && _obstructions[i].distance < closestHit.distance && _obstructions[i].distance > 0)
-                    {
-                        closestHit = _obstructions[i];
-                    }
-                }
-
-                // If obstructions detecter
-                if (closestHit.distance < Mathf.Infinity)
-                {
-                    _distanceIsObstructed = true;
-                    _currentDistance = Mathf.Lerp(_currentDistance, closestHit.distance, 1 - Mathf.Exp(-ObstructionSharpness * deltaTime));
-                }
-                // If no obstruction
-                else
-                {
-                    _distanceIsObstructed = false;
-                    _currentDistance = Mathf.Lerp(_currentDistance, TargetDistance, 1 - Mathf.Exp(-DistanceMovementSharpness * deltaTime));
-                }
-            }
-
-            // Find the smoothed camera orbit position
-            Vector3 targetPosition = _currentFollowPosition - ((targetRotation * Vector3.forward) * _currentDistance);
-
-            // Handle framing
-            targetPosition += Transform.right * FollowPointFraming.x;
-            targetPosition += Transform.up * FollowPointFraming.y;
-
-            // Apply position
-            Transform.position = targetPosition;
-        }
+        // The camera should now be in the right position, so just have it look at the focus point.
+        transform.LookAt(m_focusPosition);
     }
 }
--- Phantoms/Assets/Scripts/Player/Player.cs
+++ Phantoms/Assets/Scripts/Player/Player.cs
@@ -16,13 +16,7 @@ public class Player : MonoBehaviour
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-
-        // Tell camera to follow transform
-        CharacterCamera.SetFollowTransform(Character.CameraFollowPoint);
-
-        // Ignore the character's collider(s) for camera obstruction checks
-        CharacterCamera.IgnoredColliders.Clear();
-        CharacterCamera.IgnoredColliders.AddRange(Character.GetComponentsInChildren<Collider>());
+        CharacterCamera.Player = Character.CameraFollowPoint;
     }
 
     private void Update()
@@ -32,37 +26,7 @@ public class Player : MonoBehaviour
             Cursor.lockState = CursorLockMode.Locked;
         }
 
-        HandleCameraInput();
         HandleCharacterInput();
-    }
-
-    private void HandleCameraInput()
-    {
-        // Create the look input vector for the camera
-        float mouseLookAxisUp = Input.GetAxisRaw(MouseYInput);
-        float mouseLookAxisRight = Input.GetAxisRaw(MouseXInput);
-        Vector3 lookInputVector = new Vector3(mouseLookAxisRight, mouseLookAxisUp, 0f);
-
-        // Prevent moving the camera while the cursor isn't locked
-        if (Cursor.lockState != CursorLockMode.Locked)
-        {
-            lookInputVector = Vector3.zero;
-        }
-
-        // Input for zooming the camera (disabled in WebGL because it can cause problems)
-        float scrollInput = -Input.GetAxis(MouseScrollInput);
-#if UNITY_WEBGL
-        scrollInput = 0f;
-#endif
-
-        // Apply inputs to the camera
-        CharacterCamera.UpdateWithInput(Time.deltaTime, scrollInput, lookInputVector);
-
-        // Handle toggling zoom level
-        if (Input.GetMouseButtonDown(1))
-        {
-            CharacterCamera.TargetDistance = (CharacterCamera.TargetDistance == 0f) ? CharacterCamera.DefaultDistance : 0f;
-        }
     }
 
     private void HandleCharacterInput()
@@ -72,7 +36,7 @@ public class Player : MonoBehaviour
         // Build the CharacterInputs struct
         characterInputs.MoveAxisForward = Input.GetAxisRaw(VerticalInput);
         characterInputs.MoveAxisRight = Input.GetAxisRaw(HorizontalInput);
-        characterInputs.CameraRotation = CharacterCamera.Transform.rotation;
+        characterInputs.CameraRotation = CharacterCamera.transform.rotation;
         characterInputs.JumpDown = Input.GetKeyDown(KeyCode.Space);
         characterInputs.CrouchDown = Input.GetKeyDown(KeyCode.C);
         characterInputs.CrouchUp = Input.GetKeyUp(KeyCode.C);
