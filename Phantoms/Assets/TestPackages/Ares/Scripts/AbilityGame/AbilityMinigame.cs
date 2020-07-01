e33653789736e6b3f952ba3812ee922f4d4fd920
CJ Desktop <cjlegato@gmail.com>
2020-07-01 17:38:20 -0400

    Moved Ares into a different folder, and modifed the Battle script in ares as well as the ability data and ability editor so that an ability can now have a reference to a minigame, with the attack halting until the minigame is completed.

---
+++ Phantoms/Assets/TestPackages/Ares/Scripts/AbilityGame/AbilityMinigame.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+
+public abstract class AbilityMinigame : MonoBehaviour
+{
+    /// Enums, used to indicate the state and result of the minigame. ///
+    public enum MinigameState
+    {
+        INACTIVE,
+        INITIALIZING,
+        RUNNING,
+        FINISHED
+    }
+    public enum MinigameResult
+    {
+        FAIL,
+        SUCCESS,
+        PERFECT
+    }
+    protected MinigameState m_state = MinigameState.INACTIVE;
+    public MinigameState State
+    {
+        get { return m_state; }
+    }
+    protected MinigameResult m_result = MinigameResult.SUCCESS;
+    public MinigameResult Result
+    {
+        get { return m_result; }
+    }
+
+    /// Editor fields for general items to all minigames ///
+    [Header("General Serialize fields")]
+    [SerializeField]
+    protected bool m_startOnEnable = false;
+
+
+    protected void Awake()
+    {
+        m_state = MinigameState.INACTIVE;
+        m_result = MinigameResult.SUCCESS; // Not sure what default should be?
+    }
+
+    protected void Update()
+    {
+        switch (m_state)
+        {
+            case MinigameState.INACTIVE:
+                break;
+            case MinigameState.INITIALIZING:
+                InitializingState();
+                break;
+            case MinigameState.RUNNING:
+                RunningState();
+                break;
+            case MinigameState.FINISHED:
+                FinishedState();
+                break;
+        }
+    }
+
+    public void StartMinigame()
+    {
+        m_state = MinigameState.INITIALIZING;
+        Restart();
+    }
+
+    public void ResetMinigame()
+    {
+        m_state = MinigameState.INACTIVE;
+    }
+
+    protected abstract void Restart();
+
+    protected abstract void InitializingState();
+
+    protected abstract void RunningState();
+
+    protected abstract void FinishedState();
+}
---
+++ Phantoms/Assets/TestPackages/Ares/Scripts/AbilityGame/TestMinigame.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMinigame : AbilityMinigame
+{
+    [Header("UI References")]
+    [SerializeField]
+    private Animator m_minigameAnim = null;
+
+    [Header("Minigame Variables")]
+    [SerializeField]
+    private float m_revealTime = 0.8f;
+
+    [SerializeField]
+    private float m_maxErrorTime = 0.3f;
+
+    [SerializeField]
+    private float m_minErrorTime = 0.05f;
+
+    [SerializeField]
+    private int m_numSteps = 3;
+
+
+    private float m_currentTime = 0f;
+    private AnimatorStateInfo m_animatorState;
+    private float m_timePerStep = 0f;
+    private int m_currentStep = 0;
+
+    private void Start()
+    {
+        m_timePerStep = m_revealTime / (float)(m_numSteps - 1);
+    }
+
+    protected override void Restart()
+    {
+        m_minigameAnim.SetInteger("Step", 0);
+        m_currentTime = 0f;
+        m_currentStep = 0;
+        m_minigameAnim.SetTrigger("Start");
+    }
+
+    protected override void InitializingState()
+    {
+        m_animatorState = m_minigameAnim.GetCurrentAnimatorStateInfo(0);
+            
+        if (m_animatorState.IsName("Enter") && m_animatorState.normalizedTime >= 0.95)
+        {
+            m_minigameAnim.ResetTrigger("Start");
+            m_state = MinigameState.RUNNING;
+        }
+    }
+
+    protected override void RunningState()
+    {
+        if (m_currentStep == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                m_currentStep += 1;
+                m_minigameAnim.SetInteger("Step", m_currentStep);
+                m_currentTime = 0f;
+            }
+            return;
+        }
+
+        float prevTime = m_currentTime;
+        m_currentTime += Time.deltaTime;
+
+        if (m_currentTime >= (m_timePerStep * (float)m_currentStep))
+        {
+            m_currentStep += 1;
+            m_minigameAnim.SetInteger("Step", m_currentStep);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            if ((m_currentTime < m_revealTime) || (m_currentTime > (m_revealTime + m_maxErrorTime)))
+            {
+                m_result = MinigameResult.FAIL;
+            }
+            // If either we are within the first reveal time or this is the first frame of the reveal, it's perfect
+            else if ((m_currentTime <= (m_revealTime + m_minErrorTime)) || (prevTime < m_revealTime))
+            {
+                m_result = MinigameResult.PERFECT;
+            }
+            else
+            {
+                m_result = MinigameResult.SUCCESS;
+            }
+
+            Debug.Log("Minigame Finished with result: " + m_result + "\nRelease Time: " + m_currentTime);
+            m_minigameAnim.SetTrigger("Finish");
+            
+            m_state = MinigameState.FINISHED;
+        }
+    }
+
+    protected override void FinishedState()
+    {
+
+    }
+}
