225600830f7d985e88ba139d0761403127c31913
CJ Desktop <cjlegato@gmail.com>
2020-12-15 14:25:43 -0500

    Finished setting up the meter fill timing minigame!

--- Phantoms/Assets/TestPackages/Ares/Scripts/AbilityGame/TimingMeterMinigame.cs
+++ Phantoms/Assets/TestPackages/Ares/Scripts/AbilityGame/TimingMeterMinigame.cs
@@ -1,38 +1,62 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TimingMeterMinigame : AbilityMinigame
 {
+    [System.Serializable]
+    public class TimingMeterTargetData
+    {
+        [Range(0f, 1f)]
+        public float TargetPercent = 1f;
+        public MinigameInput InputRequired = MinigameInput.DOWN;
+    }
+
     public class TimingMeterTarget
     {
-        [Range(0f,1f)]
-        public float TargetPercent = 1f; 
+        public TimingMeterTargetData Data;
+        public MinigameResult result = MinigameResult.FAIL;
+        public InputIndicator Indicator = null;
 
-        public MinigameInput InputRequired = MinigameInput.DOWN;
-
-        public bool hit = false;
+        public TimingMeterTarget(TimingMeterTargetData data)
+        {
+            Data = data;
+            result = MinigameResult.FAIL;
+        }
     }
+
 
     [Header("Minigame Variables")]
     public float MeterDuration = 1.5f;
 
-    public float TargetTimeWindow = 0.25f;
+    public float TargetTimeWindow = 0.35f;
+    public float PerfectTimeWindow = 0.05f;
 
-    public List<TimingMeterTarget> Targets = new List<TimingMeterTarget>();
+    public int AllowedMisses = 0;
+
+    public List<TimingMeterTargetData> Targets = new List<TimingMeterTargetData>();
 
     [Header("References to parts of the Meter")]
     [SerializeField]
-    private Transform m_fillArea = null;
+    private RectTransform m_fillArea = null;
 
     [SerializeField]
     private Image m_meterFillImage = null;
+
+    [Header("Target Prefab")]
+    [SerializeField]
+    private GameObject m_inputIndicator;
 
 
     /// Private variables
     private float m_currentTime = 0f;
     private int m_currentTargetIndex = 0;
+
+    private float m_targetPercentArea = 0f;
+
+    private List<TimingMeterTarget> m_targetInstances = new List<TimingMeterTarget>();
+    private List<GameObject> m_inputIndicators = new List<GameObject>();
 
     protected override void Restart()
     {
@@ -41,14 +65,37 @@ public class TimingMeterMinigame : AbilityMinigame
 
     protected override void InitializingState()
     {
+        // Calculate some stuff
+        m_targetPercentArea = TargetTimeWindow / MeterDuration;
+
+        float targetWidth = m_targetPercentArea * m_fillArea.rect.width;
+
         // Set up the targets so they are listed in order, and set them all to not being hit.
-        Targets.Sort((t1, Texture2D)=>t1.TargetPercent.CompareTo(Texture2D.TargetPercent));
-        foreach(TimingMeterTarget target in Targets)
+        foreach (TimingMeterTargetData data in Targets)
         {
-            target.hit = false;
+            m_targetInstances.Add(new TimingMeterTarget(data));
         }
 
-        // Instantiate the targets
+        m_targetInstances.Sort((t1, t2) => t1.Data.TargetPercent.CompareTo(t2.Data.TargetPercent));
+
+        foreach (TimingMeterTarget target in m_targetInstances)
+        {
+            // Instantiate a target
+            GameObject indicator = GetInputIndicator();
+
+            // Move it to the right position
+            RectTransform indicatorTransform = indicator.GetComponent<RectTransform>();
+            indicatorTransform.anchorMin = new Vector2(target.Data.TargetPercent, 0.5f);
+            indicatorTransform.anchorMax = new Vector2(target.Data.TargetPercent, 0.5f);
+
+            //Set the width based on the time window and size of the fill area.
+            indicatorTransform.sizeDelta = new Vector2(targetWidth, targetWidth);
+
+            // Set the reference
+            target.Indicator = indicator.GetComponent<InputIndicator>();
+            target.Indicator.SetState(InputIndicator.IndicatorState.WAITING);
+            target.Indicator.SetIndicator(target.Data.InputRequired);
+        }
 
         m_currentTargetIndex = 0;
         m_currentTime = 0f;
@@ -62,16 +109,96 @@ public class TimingMeterMinigame : AbilityMinigame
         float prevTime = m_currentTime;
         m_currentTime += Time.deltaTime;
 
+        float prevProgress = prevTime / MeterDuration;
         float progress = m_currentTime / MeterDuration;
-        progress = Mathf.Clamp01(progress);
 
-        m_meterFillImage.fillAmount = progress;
+        TimingMeterTarget target = null;
+        if (m_currentTargetIndex < m_targetInstances.Count) { target = m_targetInstances[m_currentTargetIndex]; }
 
+        // Check if we hit the target (if there is any left)
+        if (target != null)
+        {
+            float minProgress = target.Data.TargetPercent - (m_targetPercentArea * 0.5f);
+            float maxProgress = target.Data.TargetPercent + (m_targetPercentArea * 0.5f);
 
+            if (prevProgress < minProgress && progress >= minProgress)
+            {
+                // First entered target zone
+                target.Indicator.SetState(InputIndicator.IndicatorState.READY_FOR_INPUT);
+            }
+
+            // Check if too late (Make sure we had at least one frame while in the target zone - unlikely but possible?)
+            if (prevProgress >= minProgress && progress > maxProgress)
+            {
+                target.result = MinigameResult.FAIL;
+                target.Indicator.SetState(InputIndicator.IndicatorState.FAILED);
+                m_currentTargetIndex += 1;
+            }
+            // Otherwise, check input
+            else if (MinigameInputManager.CheckInputDown(target.Data.InputRequired))
+            {
+                if (progress < minProgress)
+                {
+                    target.result = MinigameResult.FAIL;
+                    target.Indicator.SetState(InputIndicator.IndicatorState.FAILED);
+                }
+                else
+                {
+                    target.result = MinigameResult.SUCCESS;
+                    target.Indicator.SetState(InputIndicator.IndicatorState.INPUT_RECIEVED);
+                }
+                m_currentTargetIndex += 1;
+            }
+        }
+
+        m_meterFillImage.fillAmount = Mathf.Clamp01(progress);
+
+        if ((progress >= 1f) && (m_currentTargetIndex >= m_targetInstances.Count))
+        {
+            SetResult();
+            m_state = MinigameState.FINISHED;
+        }
+    }
+
+    private void SetResult()
+    {
+        int misses = 0;
+        foreach (TimingMeterTarget target in m_targetInstances)
+        {
+            if (target.result == MinigameResult.FAIL)
+            {
+                misses += 1;
+            }
+        }
+
+        if (misses > AllowedMisses)
+        {
+            m_result = MinigameResult.FAIL;
+        }
+        else
+        {
+            m_result = MinigameResult.SUCCESS;
+        }
     }
 
     protected override void FinishedState()
     {
 
+    }
+
+    private GameObject GetInputIndicator()
+    {
+        foreach (GameObject indicator in m_inputIndicators)
+        {
+            if (!indicator.activeSelf)
+            {
+                indicator.SetActive(true);
+                return indicator;
+            }
+        }
+
+        GameObject newIndicator = Instantiate(m_inputIndicator, m_fillArea);
+        m_inputIndicators.Add(newIndicator);
+        return newIndicator;
     }
 }
---
+++ Phantoms/Assets/TestPackages/Ares/Scripts/UI/InputIndicator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputIndicator : MonoBehaviour
+{
+    
+    public enum IndicatorState
+    {
+        DISABLED,
+        WAITING,
+        READY_FOR_INPUT,
+        INPUT_RECIEVED,
+        FAILED
+    }
+
+    private IndicatorState m_currentState = IndicatorState.DISABLED;
+    public IndicatorState CurrentState
+    {
+        get { return m_currentState; }
+        set { SetState(value); }
+    }
+
+    [SerializeField]
+    private Image m_indicatorImage = null;
+
+    public void SetIndicator(MinigameInput inputType)
+    {
+        // Set the image based on the button?
+    }
+
+    public void SetState(IndicatorState newState)
+    {
+        /// For now, just set the color based on the indicator state.
+        switch (newState)
+        {
+            case IndicatorState.DISABLED:
+                m_indicatorImage.color = new Color(0.3f, 0.3f, 0.3f, 0.5f);
+                break;
+            case IndicatorState.WAITING:
+                m_indicatorImage.color = new Color(0.5f, 0.5f, 0.5f, 0.75f);
+                break;
+            case IndicatorState.READY_FOR_INPUT:
+                m_indicatorImage.color = new Color(1f, 1f, 1f, 1f);
+                break;
+            case IndicatorState.INPUT_RECIEVED:
+                m_indicatorImage.color = new Color(0f, 1f, 0f, 1f);
+                break;
+            case IndicatorState.FAILED:
+                m_indicatorImage.color = new Color(1f, 0f, 0f, 1f);
+                break;
+        }
+
+        m_currentState = newState;
+    }
+}
