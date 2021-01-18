36eb841f850fdb8b547602acdd777d247ad302c5
CJ Desktop <cjlegato@gmail.com>
2021-01-18 10:27:42 -0500

    Moving around the ability data, added a new folder for item data.

---
+++ Phantoms/Assets/Scripts/Items/ItemData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ares;
+
+[CreateAssetMenu(menuName = "Phantoms/Item Data")]
+public class ItemData : ScriptableObject
+{
+    public Item AresData = null; 
+}
