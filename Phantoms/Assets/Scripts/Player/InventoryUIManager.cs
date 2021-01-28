63374b0d117ae017453474f313e8620b5645874f
CJ Desktop <cjlegato@gmail.com>
2021-01-28 11:54:13 -0500

    Adding in the start of a player inventory, starting to set up the prefab.

---
+++ Phantoms/Assets/Scripts/Player/InventoryUIManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUIManager : MonoBehaviour
+{
+    private enum InventoryTab
+    {
+        Items,
+        Phantoms,
+    }
+}
