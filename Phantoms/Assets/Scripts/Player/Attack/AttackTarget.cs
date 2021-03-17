ba90c51b3b19169501ed8495b69756880c59b08b
CJ Desktop <cjlegato@gmail.com>
2021-03-17 13:47:04 -0400

    Reducing prayer MP recovery from 3 to 2, and making it so cut grass stays cut when returning from the battle.

--- Phantoms/Assets/Scripts/Player/Attack/AttackTarget.cs
+++ Phantoms/Assets/Scripts/Player/Attack/AttackTarget.cs
@@ -5,11 +5,32 @@ using UnityEngine.Events;
 
 public class AttackTarget : MonoBehaviour
 {
+    [SerializeField]
+    private bool m_onlyAttackOnce = false;
+
     public UnityEvent AttackedEvent = new UnityEvent();
+
+    [Tooltip("For things that should only be attacked once, and we want to remain attacked on re-enabling.")]
+    public UnityEvent OnAttackedReEnableEvent = new UnityEvent();
+
+
+    private bool m_attacked = false;
 
     public void Attacked()
     {
         Debug.Log("Attacked " + name);
-        AttackedEvent.Invoke();
+        if (!m_onlyAttackOnce || !m_attacked)
+        {
+            AttackedEvent.Invoke();
+        }
+        m_attacked = true;
+    }
+
+    private void OnEnable()
+    {
+        if (m_onlyAttackOnce && m_attacked)
+        {
+            OnAttackedReEnableEvent.Invoke();
+        }
     }
 }
