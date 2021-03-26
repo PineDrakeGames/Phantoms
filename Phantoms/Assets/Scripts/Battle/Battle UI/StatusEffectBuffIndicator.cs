68f7e0cfb4f0399efa93f9fb05c7aec3bd33947e
CJ Desktop <cjlegato@gmail.com>
2021-03-26 12:52:57 -0400

    Reorganizing folders a bit, and adding in some in-progress stuff for listing all of the afflictions of an actor

---
+++ Phantoms/Assets/Scripts/Battle/Battle UI/StatusEffectBuffIndicator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StatusEffectBuffIndicator : StatusEffectIndicator
+{
+    [Header("Buff Data Display")]
+    [SerializeField]
+    private TMP_Text m_buffDurationText = null;
+    [SerializeField]
+    private TMP_Text m_buffAmountText = null;
+}
