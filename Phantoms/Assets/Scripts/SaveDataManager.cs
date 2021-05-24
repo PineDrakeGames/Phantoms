eefdadb314404bc691d9db3b0eb764040b7935f8
CJ Desktop <cjlegato@gmail.com>
2021-05-24 15:25:56 -0400

    Made a rough save data manager for game flags, and added another one for the free grimlee that you get

---
+++ Phantoms/Assets/Scripts/SaveDataManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveDataManager
+{
+    // TODO: Actually save stuff! For now, just storing it statically.
+    private static Dictionary<string, bool> m_flags = new Dictionary<string, bool>();
+    
+    public static bool CheckFlag(string flagName)
+    {
+        if (string.IsNullOrEmpty(flagName))
+        {
+            return false;
+        }
+
+        if (m_flags.ContainsKey(flagName) && m_flags[flagName])
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static void SetFlag(string flagName, bool flagValue = true)
+    {
+        if (string.IsNullOrEmpty(flagName))
+        {
+            return;
+        }
+
+        if (m_flags.ContainsKey(flagName))
+        {
+            m_flags[flagName] = flagValue;
+        }
+        else
+        {
+            m_flags.Add(flagName, flagValue);
+        }
+    }
+}
