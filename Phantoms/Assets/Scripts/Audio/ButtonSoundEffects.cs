9be476bb90d3acac9019e2afc0484abd796e152c
CJ Desktop <cjlegato@gmail.com>
2021-07-05 15:56:31 -0400

    Setting up some of the overworld sound effects, specifically the overworld menu ones!

---
+++ Phantoms/Assets/Scripts/Audio/ButtonSoundEffects.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Button))]
+public class ButtonSoundEffects : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    private string m_buttonHoverSoundID = "DEFAULT_HOVER";
+
+    [SerializeField]
+    private string m_buttonClickSoundID = "DEFAULT_CLICK";
+
+    private Button m_buttonComponent = null;
+
+    ///////////////////////
+    /// Unity Functions ///
+    ///////////////////////
+    private void Awake()
+    {
+        m_buttonComponent = GetComponent<Button>();
+        m_buttonComponent.onClick.AddListener(OnClick);
+    }
+
+    public void OnClick()
+    {
+        AudioManager.PlaySound(m_buttonClickSoundID);
+    }
+
+    public void OnHover()
+    {
+        if (!string.IsNullOrEmpty(m_buttonHoverSoundID) && m_buttonComponent.interactable)
+        {
+            AudioManager.PlaySound(m_buttonHoverSoundID);
+        }
+    }
+
+    public void OnStopHover()
+    {
+        // I dunno
+    }
+
+    //////////////////////////
+    /// IPointer functions ///
+    //////////////////////////
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnHover();
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OnStopHover();
+    }
+}
