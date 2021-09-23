93c79530da78420b5786ffad6eef8e089b6b520a
CJ Desktop <cjlegato@gmail.com>
2021-09-23 13:26:24 -0400

    Made some tweaks so that the tutorial thing doesn't cover the buttons, and made it so the background fades out and the button fades in only once the title scene loads.

--- Phantoms/Assets/Scripts/Title Screen/TitleScreenManager.cs
+++ Phantoms/Assets/Scripts/Title Screen/TitleScreenManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TitleScreenManager : MonoBehaviour
@@ -9,10 +10,26 @@ public class TitleScreenManager : MonoBehaviour
     [Scene]
     private string m_starterScene = null;
 
+    [SerializeField]
+    private Image m_backing = null;
+    [SerializeField]
+    private CanvasGroup m_buttons = null;
+
+    [SerializeField]
+    private float m_backingFadeOutTime = 1f;
+    [SerializeField]
+    private float m_buttonFadeInTime = 0.3f;
+
+
     private Coroutine m_currentSequence = null;
 
     private void Awake()
     {
+        m_backing.gameObject.SetActive(true);
+        Color backingColor = m_backing.color;
+        backingColor.a = 1f;
+        m_backing.color = backingColor;
+        m_buttons.gameObject.SetActive(false);
         m_currentSequence = StartCoroutine(StartingSequence());
     }
 
@@ -20,5 +37,26 @@ public class TitleScreenManager : MonoBehaviour
     {
         yield return SceneManager.LoadSceneAsync(m_starterScene, LoadSceneMode.Additive);
         OverworldManager.Instance.SetOverworldActive(false);
+
+        float currentTime = 0f;
+        while (currentTime < m_backingFadeOutTime)
+        {
+            Color backingColor = m_backing.color;
+            backingColor.a = 1f - Mathf.Clamp01(currentTime / m_backingFadeOutTime);
+            m_backing.color = backingColor;
+            yield return null;
+            currentTime += Time.deltaTime;
+        }
+        m_backing.gameObject.SetActive(false);
+        m_buttons.gameObject.SetActive(true);
+        currentTime = 0f;
+        while (currentTime < m_buttonFadeInTime)
+        {
+            m_buttons.alpha = Mathf.Clamp01(currentTime / m_buttonFadeInTime);
+            yield return null;
+            currentTime += Time.deltaTime;
+        }
+
+        m_buttons.alpha = 1f;
     }
 }
