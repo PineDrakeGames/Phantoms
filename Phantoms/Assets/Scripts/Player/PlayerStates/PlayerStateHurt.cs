9aea7eee09085104c7a53c4174d4078b303b5fb6
CJ Desktop <cjlegato@gmail.com>
2021-10-26 16:59:37 -0400

    Making it so the player hurt state now triggers the hurt animations (plays some battle sounds too) Also made some test changes to the demo scene

--- Phantoms/Assets/Scripts/Player/PlayerStates/PlayerStateHurt.cs
+++ Phantoms/Assets/Scripts/Player/PlayerStates/PlayerStateHurt.cs
@@ -10,25 +10,34 @@ public class PlayerStateHurt : PlayerMovementState
 
     private Vector3 m_startingPosition = Vector3.zero;
     private float m_currentRespawnTime = 0f;
+    bool finishedFading = false;
 
-    private const float MAX_RESPAWN_MOVE_DISTANCE = 15f;
-    private const float RESPAWN_DURATION = 0.65f;
+    private const float MAX_RESPAWN_MOVE_DISTANCE = 25f;
 
+    private const float MOVING_DURATION = 0.3f;
+    private const float FADE_OUT_DURATION = 0.75f;
+    private const float FADE_IN_DURATION = 1f;
+
+    private enum HurtSubState
+    {
+        FADING_OUT,
+        MOVING,
+        FADING_IN
+    }
+
+    private HurtSubState State = HurtSubState.FADING_OUT;
 
     public override void StateEnter()
     {
-        Controller.CharacterAnimator.SetTrigger("Fall");
-        Controller.CharacterAnimator.SetTrigger("Landed");
+        Controller.CharacterAnimator.ResetTrigger("Respawn");
+        Controller.CharacterAnimator.SetTrigger("Damage");
+
+        AudioManager.PlaySound("BATTLE_HIT");
 
         float distance = Vector3.Distance(Controller.transform.position, TargetPosition);
         if (distance > MAX_RESPAWN_MOVE_DISTANCE)
         {
-            Controller.Motor.SetPosition(TargetPosition);
             MoveToTarget = false;
-            if (LookAtTarget)
-            {
-                Controller.Motor.SetRotation(TargetRotation);
-            }
         }
         else
         {
@@ -36,7 +45,9 @@ public class PlayerStateHurt : PlayerMovementState
             MoveToTarget = true;
         }
 
+        State = HurtSubState.FADING_OUT;
         m_currentRespawnTime = 0f;
+        finishedFading = false;
     }
 
     public override void TickInput(PlayerCharacterInputs input)
@@ -65,32 +76,66 @@ public class PlayerStateHurt : PlayerMovementState
 
         m_currentRespawnTime += Time.deltaTime;
 
-        if (MoveToTarget)
+        switch (State)
         {
-            float progress = Mathf.Clamp01(m_currentRespawnTime / RESPAWN_DURATION);
-            // Faster at start, slower at end - exponential change
-            float easeInProgress = 1f - (Mathf.Pow((1f - progress), 2f));
-            Vector3 position = Vector3.Lerp(m_startingPosition, TargetPosition, easeInProgress);
-            Controller.Motor.SetPosition(position);
-        }
+            case HurtSubState.FADING_OUT:
+                if (m_currentRespawnTime >= FADE_OUT_DURATION)
+                {
+                    if (!MoveToTarget)
+                    {
+                        Controller.Motor.SetPosition(TargetPosition);
+                        if (LookAtTarget)
+                        {
+                            Controller.Motor.SetRotation(TargetRotation);
+                        }
+                    }
+                    State = HurtSubState.MOVING;
+                    Controller.CharacterAnimator.SetTrigger("Fall");
+                    Controller.CharacterAnimator.SetTrigger("Landed");
+                    m_currentRespawnTime = 0f;
+                }
+                break;
 
-        if (m_currentRespawnTime >= RESPAWN_DURATION)
-        {
-            if (MoveToTarget)
-            {
-                Controller.Motor.SetPosition(TargetPosition);
-            }
-            if (LookAtTarget)
-            {
-                Controller.Motor.SetRotation(TargetRotation);
-            }
+            case HurtSubState.MOVING:
+                if (MoveToTarget)
+                {
+                    float progress = Mathf.Clamp01(m_currentRespawnTime / MOVING_DURATION);
+                    // Faster at start, slower at end - exponential change
+                    float easeInProgress = 1f - (Mathf.Pow((1f - progress), 2f));
+                    Vector3 position = Vector3.Lerp(m_startingPosition, TargetPosition, easeInProgress);
+                    Controller.Motor.SetPosition(position);
+                }
+                if (m_currentRespawnTime >= MOVING_DURATION)
+                {
+                    if (MoveToTarget)
+                    {
+                        Controller.Motor.SetPosition(TargetPosition);
+                    }
+                    if (LookAtTarget)
+                    {
+                        Controller.Motor.SetRotation(TargetRotation);
+                    }
 
-            Controller.SetState(new PlayerStateIdle());
+                    State = HurtSubState.FADING_IN;
+                    Controller.CharacterAnimator.SetTrigger("Respawn");
+                    AudioManager.PlaySound("BATTLE_HEAL");
+                    m_currentRespawnTime = 0f;
+                }
+                break;
+
+            case HurtSubState.FADING_IN:
+
+                if (!finishedFading && m_currentRespawnTime >= FADE_IN_DURATION)
+                {
+                    finishedFading = true;
+                    Controller.SetState(new PlayerStateIdle());
+                }
+                break;
         }
     }
 
     public override void StateExit()
     {
-
+        Controller.CharacterAnimator.SetTrigger("Respawn");
     }
 }
